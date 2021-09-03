a4834b051521df5a5ac8221eca3f4b37c7b68260
Avaer Kazmer <a@modules.io>
2021-09-03 13:20:32 -0400

    Update API

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import * as THREE from 'three';
 // import {BufferGeometryUtils} from 'BufferGeometryUtils';
-import {scene, renderer, camera, runtime, world, physics, ui, rig, app, appManager} from 'app';
+// import {scene, renderer, camera, runtime, world, physics, ui, rig, app, appManager} from 'app';
 import {Sky} from './Sky.js';
+import metaversefile from 'metaversefile';
+const {useFrame, useLocalPlayer} = metaversefile;
 
 const localVector = new THREE.Vector3();
 const localVector2 = new THREE.Vector3();
@@ -14,42 +16,42 @@ const localRay = new THREE.Ray();
 const localColor = new THREE.Color();
 const localColor2 = new THREE.Color();
 
-const effectController = {
-  turbidity: 20,
-  rayleigh: 10,
-  mieCoefficient: 0.1,
-  mieDirectionalG: 0.9999,
-  inclination: 0, // elevation / inclination
-  azimuth: 0, // Facing front,
-  // exposure: renderer.toneMappingExposure
+export default () => {
+  const effectController = {
+    turbidity: 20,
+    rayleigh: 10,
+    mieCoefficient: 0.1,
+    mieDirectionalG: 0.9999,
+    inclination: 0, // elevation / inclination
+    azimuth: 0, // Facing front,
+    // exposure: renderer.toneMappingExposure
+  };
+  const sun = new THREE.Vector3();
+  useFrame(() => {
+    var uniforms = skybox.material.uniforms;
+    uniforms.turbidity.value = effectController.turbidity;
+    uniforms.rayleigh.value = effectController.rayleigh;
+    uniforms.mieCoefficient.value = effectController.mieCoefficient;
+    uniforms.mieDirectionalG.value = effectController.mieDirectionalG;
+
+    const {position} = useLocalPlayer().leftHand;
+    // effectController.azimuth = (0.05 + ((Date.now() / 1000) * 0.1)) % 1;
+    effectController.azimuth = Math.min(Math.max(-position.z / 30, -0.3), 0.3);
+    var theta = Math.PI * (effectController.inclination - 0.5);
+    var phi = 2 * Math.PI * (effectController.azimuth - 0.5);
+
+    sun.x = Math.cos(phi);
+    sun.y = Math.sin(phi) * Math.sin(theta);
+    sun.z = Math.sin(phi) * Math.cos(theta);
+
+    uniforms.sunPosition.value.copy(sun);
+  });
+
+  const skybox = new Sky();
+  skybox.scale.setScalar(1000);
+  // skybox.update = update;
+  // skybox.update();
+  // app.object.add(skybox);
+
+  return skybox;
 };
-const sun = new THREE.Vector3();
-function update() {
-  var uniforms = skybox.material.uniforms;
-  uniforms.turbidity.value = effectController.turbidity;
-  uniforms.rayleigh.value = effectController.rayleigh;
-  uniforms.mieCoefficient.value = effectController.mieCoefficient;
-  uniforms.mieDirectionalG.value = effectController.mieDirectionalG;
-
-  const transforms = rig.getRigTransforms();
-  const {position} = transforms[0];
-  // effectController.azimuth = (0.05 + ((Date.now() / 1000) * 0.1)) % 1;
-  effectController.azimuth = Math.min(Math.max(-position.z / 30, -0.3), 0.3);
-  var theta = Math.PI * (effectController.inclination - 0.5);
-  var phi = 2 * Math.PI * (effectController.azimuth - 0.5);
-
-  sun.x = Math.cos(phi);
-  sun.y = Math.sin(phi) * Math.sin(theta);
-  sun.z = Math.sin(phi) * Math.cos(theta);
-
-  uniforms.sunPosition.value.copy(sun);
-}
-const skybox = new Sky();
-skybox.scale.setScalar(1000);
-skybox.update = update;
-skybox.update();
-app.object.add(skybox);
-
-app.addEventListener('frame', () => {
-  update();
-});
